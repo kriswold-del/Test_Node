ebf4795f128e68d3784ec778b932e96874cd27dd
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-04-07 13:19:38 +0100

    test

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 console.log("here");
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set('views', path.join(__dirname, 'www'))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
